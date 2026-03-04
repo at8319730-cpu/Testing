9e4ede449d49d10fc4b581a1f8d183c7db8ef376
Ashok Tanniru <your_email@example.com>
2026-03-04 15:48:02 +0530

    Login-Web-Page project

---
+++ Odd.java
@@ -0,0 +1,6 @@
+class Even{
+    public static void main(String [] args)
+    {
+        System.out.println("1 3 5 7");
+    }
+}
---
+++ Prime.java
@@ -0,0 +1,6 @@
+class Even{
+    public static void main(String [] args)
+    {
+        System.out.println("2 4 6 8");
+    }
+}
